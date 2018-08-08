fb14c881913f773c0e8b264f32da9e8df8861439
limgeun <shing100@naver.com>
2018-08-08 20:50:15 +0900

    유저 언팔로우 기능 추가

--- limstagram/users/urls.py
+++ limstagram/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path("explore/", view=views.ExploreUsers.as_view(), name="explore_users"),
-    path("<int:user_id>/follow", view=views.FollowUser.as_view(), name="follow_user")
+    path("<int:user_id>/follow", view=views.FollowUser.as_view(), name="follow_user"),
+    path("<int:user_id>/unfollow", view=views.UnFollowUser.as_view(), name="unfollow_user")
 ]
