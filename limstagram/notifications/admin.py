f66c1a3edcb5fb9ebdb5e5e379f46e400b424bd4
limgeun <shing100@naver.com>
2018-08-16 22:35:16 +0900

    notification fleid 추가하기 2

--- limstagram/notifications/admin.py
+++ limstagram/notifications/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.notification)
+class NotificationAdmin(admin.ModelAdmin):
+    # 어드민 리스트 보여주기
+    list_display = (
+        'creator',
+        'to',
+        'notification_type',
+    )
--- limstagram/notifications/models.py
+++ limstagram/notifications/models.py
@@ -14,4 +14,4 @@ class notification(image_models.TimeStampedModel):
     creator = models.ForeignKey(user_models.User, related_name='creator', on_delete=models.CASCADE)
     to = models.ForeignKey(user_models.User, related_name='to', on_delete=models.CASCADE)
     notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-    image = models.ForeignKey(image_models.Image, on_delete=models.CASCADE)
+    image = models.ForeignKey(image_models.Image, on_delete=models.CASCADE, null=True, blank=True)
