4bbe7314d3581266d4ebd8d3db500dbba9850a1e
limgeun <shing100@naver.com>
2018-08-21 21:13:28 +0900

    내 사진 댓글 삭제 기능 추가

--- limstagram/images/urls.py
+++ limstagram/images/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
     path("<int:image_id>/unlike/", view=views.unLikeImage.as_view(), name="unlike_image"),
-    path("<int:image_id>/comment", view=views.CommentOnImage.as_view(), name="commnet_image"),
+    path("<int:image_id>/comments", view=views.CommentOnImage.as_view(), name="commnet_image"),
+    path("<int:image_id>/comments/<int:comment_id>", view=views.ModerateComments.as_view(), name="commnet_image"),
     path("comments/<int:comment_id>", view=views.Comment.as_view(), name="comment"),
     path("search", view=views.Search.as_view(), name="search")
 ]
