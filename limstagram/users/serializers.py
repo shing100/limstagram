4bad07b86925d178a40694dc83944c3746e6f9f6
limgeun <shing100@naver.com>
2018-08-29 21:31:16 +0900

    profile image upload 기능추가

--- limstagram/users/serializers.py
+++ limstagram/users/serializers.py
@@ -4,7 +4,7 @@ from limstagram.images import serializers as images_serializers
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
-    images = images_serializers.CountImageSerializer(many=True)
+    images = images_serializers.CountImageSerializer(many=True, read_only=True)
     # 해달 필드를 ReadOnly로 필드로 지정
     post_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
