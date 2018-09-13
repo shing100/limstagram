d5ccc769aca8c833111594e54f9501d66350ff23
limgeun <shing100@naver.com>
2018-09-13 21:15:07 +0900

    NODE_PATH 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // 리덕스 추가
 import { Provider } from 'react-redux'
-import store from './redux/configureStore';
-import './index.css';
-import App from './App';
+import store from 'redux/configureStore';
+import 'index.css';
+import App from 'App';
 
 //console.log(store.getState());
 
