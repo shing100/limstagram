749e1cb10feeebf26459eec33a3e7066587ace6a
limgeun <shing100@naver.com>
2018-09-13 21:10:16 +0900

    리엑트에 리듀서 연결하기

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 리덕스 추가
+import { Provider } from 'react-redux'
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//console.log(store.getState());
+
+ReactDOM.render(
+    <Provider store={store}>
+        < App />
+    </Provider>
+    , document.getElementById('root')
+);
