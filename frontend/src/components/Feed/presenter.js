cf1d28f83bf99b2c0458c843aad9dd92146c3064
limgeun <shing100@naver.com>
2018-10-03 15:42:17 +0900

    Feed 컨테이너 추가

---
+++ frontend/src/components/Feed/presenter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropsTypes from "prop-types";
+import styles from "./styles.scss";
+
+const Feed = props => "Feed!";
+
+Feed.propsTypes = {
+    loading: PropsTypes.bool.isRequired
+}
+
+export default Feed;
