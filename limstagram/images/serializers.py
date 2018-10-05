46ec8541ba17a4290cb59890839a38e5396ccb10
limgeun <shing100@naver.com>
2018-10-05 19:00:12 +0900

    is_like 함수 추가

--- limstagram/images/serializers.py
+++ limstagram/images/serializers.py
@@ -59,6 +59,7 @@ class ImageSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True)
     creator = FeedUserSerializer()
     tags = TagListSerializerField()
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Image
@@ -71,8 +72,19 @@ class ImageSerializer(serializers.ModelSerializer):
             'like_count',
             'creator',
             'natural_time',
-            'tags'
+            'tags',
+            'is_liked'
         )
+
+    def get_is_liked(self, obj):
+        if 'request' in self.context:
+            request = self.context['request']
+            try:
+                models.Like.objects.get(creator__id=request.user.id, image__id=obj.id)
+                return True
+            except models.Like.DoesNotExist:
+                return False
+        return False
 
 class InputImageSerializer(serializers.ModelSerializer):
 
--- limstagram/images/views.py
+++ limstagram/images/views.py
@@ -29,7 +29,7 @@ class Images(APIView):
         ## sorted_list = sorted(image_list, key=get_key, reverse=True)
         sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
 
-        serializer = serializers.ImageSerializer(sorted_list, many=True)
+        serializer = serializers.ImageSerializer(sorted_list, many=True, context={'request' : request})
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -205,7 +205,7 @@ class ImageDetail(APIView):
         except models.Image.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = serializers.ImageSerializer(image)
+        serializer = serializers.ImageSerializer(image, context={'request' : request})
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
