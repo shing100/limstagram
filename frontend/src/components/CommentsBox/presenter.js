e0790f8f6d17848335993da523f3f49f0714f18d
limgeun <shing100@naver.com>
2018-10-05 18:39:16 +0900

    textarea 추가

--- frontend/src/components/CommentsBox/presenter.js
+++ frontend/src/components/CommentsBox/presenter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
 const CommentsBox = (props, context) => (
     <form className={styles.commentBox}>
-        <textarea className={styles.input} placeholder={context.t("댓글 달기...")}/>
+        <Textarea className={styles.input} placeholder={context.t("댓글 달기...")}/>
     </form>
 )
 
