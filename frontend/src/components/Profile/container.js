44e4b4db236457939b5b19fe3a78fc601adf438f
limgeun <shing100@naver.com>
2018-10-17 23:16:37 +0900

    Profile 컴포넌트 추가

---
+++ frontend/src/components/Profile/container.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Feed from "./presenter";
+
+class Container extends Component {
+    state = {
+        loading: true
+    };
+
+    static porpTypes = {
+        getFeed: PropTypes.func.isRequired,
+        feed: PropTypes.array
+    }
+
+    // feed 를 받음
+    componentWillReceiveProps = nextProps => {
+        if(nextProps.feed) {
+            this.setState({
+                loading: false,
+                feed: nextProps.feed
+            })
+        }
+    }
+
+    componentDidMount() {
+        const { getFeed } = this.props;
+        if(!this.props.feed) {
+            getFeed();
+        } else {
+            this.setState({
+                loading: false,
+            })
+        }
+    }
+    render() {
+        const { feed } = this.props;
+        return <Feed {...this.state} feed={feed}/>
+    }
+}
+
+export default Container;
---
+++ frontend/src/components/Profile/presenter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropsTypes from "prop-types";
+import styles from "./styles.scss";
+import Loading from "components/Loading";
+import FeedPhoto from "components/FeedPhoto";
+
+const Feed = props => {
+    if(props.loading) {
+        return <LoadingFeed/>
+    }else if(props.feed) {
+        return <RenderFeed {...props}/>
+    }
+};
+
+const LoadingFeed = props => (
+    <div className={styles.feed}>
+        <Loading/>
+    </div>
+);
+
+const RenderFeed = props => (
+    <div className={styles.feed}>
+        {props.feed.map(photo => <FeedPhoto {...photo} key={photo.id}/>)}
+    </div>
+)
+
+Feed.propsTypes = {
+    loading: PropsTypes.bool.isRequired,
+    feed: PropsTypes.array
+}
+
+export default Feed;
