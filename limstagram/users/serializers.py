4cff5a49f5a29abe9d4e97a5e336db1d1ca32093
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-03-03 13:03:59 +0900

    is_self, following 시리얼라이저 필드 추가

--- limstagram/users/serializers.py
+++ limstagram/users/serializers.py
@@ -12,7 +12,9 @@ class UserProfileSerializer(serializers.ModelSerializer):
     post_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
-
+    is_self = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
+    
     class Meta:
         model = models.User
         fields = (
@@ -24,8 +26,26 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "post_count",
             "followers_count",
             "following_count",
-            'images'
+            'images',
+            'is_self',
+            'following'
         )
+
+    def get_is_self(self, user):
+        if 'request' in self.context:
+            request = self context['request']
+            if user.id == request.user.id:
+                return True
+            else
+                return False
+        return False
+
+    def get_following(self, obj):
+        if 'request' in self.context:
+            request = self.context['request']
+            if obj in request.user.following.all():
+                return True
+        return False
 
 class ListUserSerializer(serializers.ModelSerializer):
 
